9d91b50b6a0d8eb40ca11b298419f35b7d30e799
Joel Campos <joelcamposoliva9000@gmail.com>
2024-07-02 18:19:30 +0200

    build: :heavy_plus_sign: Added Bootstrap

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./app.tsx";
+
 import "./index.css";
+
+// Cargamos el CSS de Bootstrap en la app
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
