258955b60fe94c895569cbead589a88f002f01cf
Joel Campos <joelcamposoliva9000@gmail.com>
2024-07-03 20:04:42 +0200

    feat: :sparkles: Functional email and password authentication

--- src/pages/login.page.tsx
+++ src/pages/login.page.tsx
@@ -1,32 +1,62 @@
-import { Button, Card, CardBody, Form } from "react-bootstrap";
+import { Alert, Button, Card, CardBody, Form } from "react-bootstrap";
 import { AuthService } from "../services/auth/auth.service";
-import { useState } from "react";
+import { useEffect, useState, useTransition } from "react";
 
 import styles from "./login.page.module.css";
+import { AuthError } from "firebase/auth";
 
 export default function LoginPage() {
+  const [isAuthenticating, setIsAuthenticating] = useState();
+
   return (
     <div className={styles.container}>
       <Card className={styles.card}>
         <CardBody>
-          <EmailAndPasswordForm />
+          <EmailAndPasswordForm
+            setIsAuthenticating={setIsAuthenticating}
+            isAuthenticating={isAuthenticating}
+          />
         </CardBody>
       </Card>
     </div>
   );
 }
 
+type FormProps = {
+  setIsAuthenticating: (isAuthenticating: boolean) => void;
+  isAuthenticating: boolean;
+};
+
 /**
  * Componente que contiene el formulario de inicio de sesión por
  * correo electrónico y contraseña.
  */
-const EmailAndPasswordForm = () => {
+const EmailAndPasswordForm = ({
+  setIsAuthenticating,
+  isAuthenticating,
+}: Readonly<FormProps>) => {
   const [emailState, setEmailState] = useState<string>();
   const [passwordState, setPasswordState] = useState<string>();
+  const [errorState, setErrorState] = useState<AuthError>();
 
-  const onLoginClick = () => {
+  const [, startTransition] = useTransition();
+
+  // Cada vez que se modifiquen el usuario o contraseña se reiniciará el estado de error
+  useEffect(() => {
+    if (errorState) setErrorState(undefined);
+  }, [emailState, passwordState]);
+
+  // Función que se llama al hacer click en login
+  const onLoginClick = async () => {
     if (emailState && passwordState) {
-      AuthService.signIn(emailState, passwordState);
+      setIsAuthenticating(true);
+
+      const authResult = await AuthService.signIn(emailState, passwordState);
+      startTransition(() => {
+        if (authResult.error) setErrorState(authResult.error);
+        else setErrorState(undefined);
+        setIsAuthenticating(false);
+      });
     }
   };
 
@@ -46,7 +76,10 @@ const EmailAndPasswordForm = () => {
           onChange={(event) => setPasswordState(event.target.value)}
         />
       </Form.Group>
-      <Button onClick={onLoginClick}>Iniciar sesión</Button>
+      {errorState && <Alert variant="danger">{errorState.message}</Alert>}
+      <Button disabled={isAuthenticating} onClick={onLoginClick}>
+        Iniciar sesión
+      </Button>
     </Form>
   );
 };
