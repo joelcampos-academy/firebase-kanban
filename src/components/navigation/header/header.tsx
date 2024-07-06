ac4674dc4d15ff6d2ef280abe62f6ce74ca606a8
Joel Campos <joelcamposoliva9000@gmail.com>
2024-07-06 16:35:11 +0200

    feat: :lipstick: Display new or old logo

--- src/components/navigation/header/header.tsx
+++ src/components/navigation/header/header.tsx
@@ -1,14 +1,29 @@
-import { Container, Nav, Navbar } from "react-bootstrap";
+import { Container, Nav, Navbar, Spinner } from "react-bootstrap";
 
 import logo from "../../../assets/branding/logo.png";
+import oldLogo from "../../../assets/branding/old-logo.png";
 
 import styles from "./header.module.css";
 import UserModal from "../../user/user-modal";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { AuthService } from "../../../services/auth/auth.service";
+import { LogoDatabaseService } from "../../../services/realtime-database/logo-database.service";
+import { getCurrentUser } from "../../../utils/auth/get-current-user.util";
 
 export default function Header() {
   const [isUserModalVisible, setUserModalVisible] = useState(false);
+  const [displayNewLogo, setDisplayNewLogo] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    // Obtenemos nuestro usuario y sacamos nuestro ID
+    const user = getCurrentUser();
+    const userId = user.uid;
+
+    // Obtenemos el código del logo de nuestro usuario
+    LogoDatabaseService.getFirebaseLogoCodeByUserId(userId).then((logoCode) => {
+      setDisplayNewLogo(logoCode === "new"); // <- Cargamos en el estado si es el nuevo logo
+    });
+  }, []);
 
   const onUserClick = () => {
     setUserModalVisible(true);
@@ -21,7 +36,13 @@ export default function Header() {
       <Navbar expand="lg" className="bg-body-tertiary">
         <Container>
           <Navbar.Brand onClick={onBrandClick} className={styles.brand}>
-            <img src={logo} alt="App logo" />
+            <div className={styles.logo}>
+              {displayNewLogo === null ? (
+                <Spinner />
+              ) : (
+                <img src={displayNewLogo ? logo : oldLogo} alt="App logo" />
+              )}
+            </div>
             Firebase Kanban
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
