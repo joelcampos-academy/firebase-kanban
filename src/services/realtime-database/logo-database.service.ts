90962abebf1691191ad8c022bc2564975768edb9
Joel Campos <joelcamposoliva9000@gmail.com>
2024-07-06 16:45:37 +0200

    feat: :sparkles: Setup listener to logo changes

--- src/services/realtime-database/logo-database.service.ts
+++ src/services/realtime-database/logo-database.service.ts
@@ -1,4 +1,4 @@
-import { get, ref } from "firebase/database";
+import { get, onValue, ref } from "firebase/database";
 import { realtimeDatabase } from "../../utils/firebase/setup-firebase.util";
 
 export class LogoDatabaseService {
@@ -13,5 +13,27 @@ export class LogoDatabaseService {
     const value: string = valueSnapshot.val();
 
     return value;
+  };
+
+  static onFirebaseLogoCodeByUserIdChanged = (
+    userId: string,
+    onChange: (logoCode: string) => void
+  ) => {
+    // Referencia al valor. Aún no hemos accedido a él. Vemos que la ruta es 'logo/{userId}'.
+    const valueRef = ref(realtimeDatabase, `logo/${userId}`);
+
+    // Usamos on value, que llamará a la función pasada por parámetro cada vez que el dato cambie en la DB
+    const unsubscribe = onValue(valueRef, (valueSnapshot) => {
+      // Esto se llama cuando Firebase detecta un cambio en la DB
+
+      // Igual que en la función getFirebaseLogoCodeByUserId, leemos el valor desde la snapshot (que recibimos por parámetro).
+      const value: string = valueSnapshot.val();
+
+      // Llamamos a la función que hemos creado y que recibimos por parámetro.
+      onChange(value);
+    });
+
+    // Unsubscribe se utiliza para detener la escucha de cambios. La devolvemos por si se desea usar.
+    return unsubscribe;
   };
 }
